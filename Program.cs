0a61c698c25c522b1185d5f97594d58e68f66ccf
fselimkondolot <selim@sambapos.com>
2023-06-07 17:59:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using FileAccessService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
